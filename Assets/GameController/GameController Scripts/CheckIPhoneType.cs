c35e8f84ed524ae48098be777321c598c51bdee8
Wm. Josiah Erikson <josiah@insanetechnology.com>
2017-11-28 23:52:18 -0500

    Added extra safety checks in the oldphone method to prevent it from running on anything other than iOS

--- Assets/GameController/GameController Scripts/CheckIPhoneType.cs
+++ Assets/GameController/GameController Scripts/CheckIPhoneType.cs
@@ -5,13 +5,18 @@ public static class CheckIPhoneType {
 
 	//determines which iphones the game will use the compressed sprites for
    public static bool OldPhone() {
-		UnityEngine.iOS.DeviceGeneration [] oldIphones = new UnityEngine.iOS.DeviceGeneration[]{UnityEngine.iOS.DeviceGeneration.iPhone, UnityEngine.iOS.DeviceGeneration.iPhone3G, UnityEngine.iOS.DeviceGeneration.iPhone3GS, 
-			UnityEngine.iOS.DeviceGeneration.iPhone4, UnityEngine.iOS.DeviceGeneration.iPhone4S, UnityEngine.iOS.DeviceGeneration.iPodTouch4Gen, UnityEngine.iOS.DeviceGeneration.iPodTouch5Gen};
-		foreach (UnityEngine.iOS.DeviceGeneration phone in oldIphones) {
-			if (UnityEngine.iOS.Device.generation == phone) {
-				return true;
-			}
-		}
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            UnityEngine.iOS.DeviceGeneration[] oldIphones = new UnityEngine.iOS.DeviceGeneration[]{UnityEngine.iOS.DeviceGeneration.iPhone, UnityEngine.iOS.DeviceGeneration.iPhone3G, UnityEngine.iOS.DeviceGeneration.iPhone3GS,
+            UnityEngine.iOS.DeviceGeneration.iPhone4, UnityEngine.iOS.DeviceGeneration.iPhone4S, UnityEngine.iOS.DeviceGeneration.iPodTouch4Gen, UnityEngine.iOS.DeviceGeneration.iPodTouch5Gen};
+            foreach (UnityEngine.iOS.DeviceGeneration phone in oldIphones)
+            {
+                if (UnityEngine.iOS.Device.generation == phone)
+                {
+                    return true;
+                }
+            }
+        }
 		return false;
 	}
 }
