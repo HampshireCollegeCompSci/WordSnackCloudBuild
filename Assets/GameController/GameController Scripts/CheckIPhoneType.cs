0085458d2920f2ef997acb6725d12ed507968798
Wm. Josiah Erikson <josiah@insanetechnology.com>
2017-11-28 21:12:41 -0500

    Removed test line in CheckIPhoneType.cs

--- Assets/GameController/GameController Scripts/CheckIPhoneType.cs
+++ Assets/GameController/GameController Scripts/CheckIPhoneType.cs
@@ -5,7 +5,6 @@ public static class CheckIPhoneType {
 
 	//determines which iphones the game will use the compressed sprites for
    public static bool OldPhone() {
-        UnityEngine.iOS.DeviceGeneration
 		UnityEngine.iOS.DeviceGeneration [] oldIphones = new UnityEngine.iOS.DeviceGeneration[]{UnityEngine.iOS.DeviceGeneration.iPhone, UnityEngine.iOS.DeviceGeneration.iPhone3G, UnityEngine.iOS.DeviceGeneration.iPhone3GS, 
 			UnityEngine.iOS.DeviceGeneration.iPhone4, UnityEngine.iOS.DeviceGeneration.iPhone4S, UnityEngine.iOS.DeviceGeneration.iPodTouch4Gen, UnityEngine.iOS.DeviceGeneration.iPodTouch5Gen};
 		foreach (UnityEngine.iOS.DeviceGeneration phone in oldIphones) {
